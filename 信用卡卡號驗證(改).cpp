2081fd0d6446a53ceb2077594cdd08fb75ae2229
marsmarsgithub <43640638+marsmarsgithub@users.noreply.github.com>
2018-11-11 21:06:52 +0800

    Update 信用卡卡號驗證(改).cpp

--- 信用卡卡號驗證(改).cpp
+++ 信用卡卡號驗證(改).cpp
@@ -5,38 +5,38 @@ void b(int);
 int main() {
   long int cardnumber;
   cout << "請輸入信用卡號:";
-  b(cardnumber);
+   b(cardnumber);
   int sixteenth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int fifteenth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int fourteenth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int thirteenth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int twelveth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int eleventh = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int tenth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int ninth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int eighth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int seventh = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int sixth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int fifth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int fourth = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int third = cardnumber;
   int w = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int second = cardnumber;
-  b(cardnumber);
+   b(cardnumber);
   int z = cardnumber;
   int y = cardnumber;
   a(z);
@@ -52,12 +52,12 @@ int main() {
     if ( y == 4 ) cout << "這是Visa card。";
     else if ( y == 5 && w <= 55 && w >= 51 ) cout << "這是Master card。"; 
     cout << "這是Master card。";
-   }
+}
     else cout << "這是其他信用卡。"; 
 }
 else cout << "這是其他信用卡。\n";
 void a(int number) {
-  if ( number * 2 >= 10 ) {
+  if ( number*2 >= 10 ) {
     number=number / 10 + number % 10;
 }
   else number=number * 2;
