7c5cdc01238ddfe44915b8b4e5d5d67277285803
marsmarsgithub <43640638+marsmarsgithub@users.noreply.github.com>
2018-11-16 19:27:52 +0800

    Update 信用卡卡號驗證(改).cpp

--- 信用卡卡號驗證(改).cpp
+++ 信用卡卡號驗證(改).cpp
@@ -1,67 +1,40 @@
 #include <iostream>
 using namespace std;
-void a(int);
-void b(int);
 int main() {
   long int cardnumber;
-  cout << "請輸入信用卡號:";
-   b(cardnumber);
-  int sixteenth = cardnumber;
-   b(cardnumber);
-  int fifteenth = cardnumber;
-   b(cardnumber);
-  int fourteenth = cardnumber;
-   b(cardnumber);
-  int thirteenth = cardnumber;
-   b(cardnumber);
-  int twelveth = cardnumber;
-   b(cardnumber);
-  int eleventh = cardnumber;
-   b(cardnumber);
-  int tenth = cardnumber;
-   b(cardnumber);
-  int ninth = cardnumber;
-   b(cardnumber);
-  int eighth = cardnumber;
-   b(cardnumber);
-  int seventh = cardnumber;
-   b(cardnumber);
-  int sixth = cardnumber;
-   b(cardnumber);
-  int fifth = cardnumber;
-   b(cardnumber);
-  int fourth = cardnumber;
-   b(cardnumber);
-  int third = cardnumber;
-  int w = cardnumber;
-   b(cardnumber);
-  int second = cardnumber;
-   b(cardnumber);
-  int z = cardnumber;
-  int y = cardnumber;
-  a(z);
-  a(third);
-  a(fifth);
-  a(seventh);
-  a(ninth);
-  a(eleventh);
-  a(thirteenth);
-  a(fifteenth);
-   int c = z + second  + third + fourth + fifth + sixth + seventh + eighth + ninth + tenth + eleventh + twelveth + thirteenth + fourteenth + fifteenth + sixteenth;
-   if ( c % 10 == 0 ) {
-    if ( y == 4 ) cout << "這是Visa card。";
-    else if ( y == 5 && w <= 55 && w >= 51 ) cout << "這是Master card。"; 
-    cout << "這是Master card。";
+  cout << "請輸入信用卡號:\n";
+  cin >> cardnumber;
+  long int card=cardnumber,card2=cardnumber/10,total=0,checknumber;
+  for (int i=0;i<8;i++){
+    int number=card%10;
+    card=card/100;
+    total=total+number;
+  }
+  for (int X=0;X<8;X++){
+    int number=card2;
+    card2=card2/100;
+  if (number>9){
+    number/10+number%10;
+  }
+  else {
+  number=number*2;
+  }
+  total=total+number;
+  }
+   for ( int y = 0 ; y < 15 ; y++ ) {
+    cardnumber = cardnumber / 10;
+  }
+  if (total % 10 == 0) {
+    checknumber = 0;
+  }
+  else {
+    checknumber = 10 - total % 10;
+  }
+  if ( total % 10 == 0 ) {
+  if ( cardnumber == 4 ) cout << "這是Visa card。";
+    else if ( cardnumber == 5 ) cout << "這是Master card。";
+    else cout << "這是其他信用卡。";
+  }
+  else cout << "這是其他信用卡。\n";
 }
-    else cout << "這是其他信用卡。"; 
-}
-else cout << "這是其他信用卡。\n";
-void a(int number) {
-  if ( number*2 >= 10 ) {
-    number=number / 10 + number % 10;
-}
-  else number=number * 2;
-}
-void b(int number) {
-  number=number % 10;
-}
+
